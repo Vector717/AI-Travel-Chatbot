99163b6f7f4f61659fe26819f6a1afee76fcb9cc
Vector <81518724+Vector717@users.noreply.github.com>
2022-07-15 22:21:30 +0530

    Add files via upload

---
+++ chatbot_python_javascript-master/nltk_utils.py
@@ -0,0 +1,22 @@
+import nltk
+nltk.download('punkt')
+import numpy as np
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
+
+# breaks sentences into words
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+# stemming words ie cuts the words into smaller words
+def stem(word):
+    return stemmer.stem(word.lower())
+
+def bag_of_words(tokenized_sentence,all_words):
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    bag = np.zeros(len(all_words), dtype = np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1
+
+    return bag
